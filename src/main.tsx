39c12fae15ba8c8360eec8f6ff4be474067949ff
yjun1806 <yjun1806@gmail.com>
2026-02-26 12:01:25 +0900

    perf: 쿼리 staleTime 30초에서 5초로 단축

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
-      staleTime: 30_000,
+      staleTime: 5_000,
       refetchOnWindowFocus: true,
     },
   },
